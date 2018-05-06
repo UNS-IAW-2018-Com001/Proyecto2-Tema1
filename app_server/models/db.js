08e5c7cffb03193f9cb5cb59c72213f9749ceaff
rochy22 <rosegovia22@gmail.com>
2018-05-06 18:33:19 -0300

    arreglo mongoose2

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbURI = process.env.MLAB_URI;
+//const dbURI = 'mongodb://localhost/scotus';
 mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', () => {
