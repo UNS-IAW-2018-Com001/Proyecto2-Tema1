429a384c264991a6bc009f36c6f73e85deccfb5f
rochy22 <rosegovia22@gmail.com>
2018-05-07 16:41:47 -0300

    post comentarios

--- app_server/controllers/preferenciasController.js
+++ app_server/controllers/preferenciasController.js
@@ -11,7 +11,8 @@ const getPreferencias=function(req,res){
 const postPreferencias=function(req,res){
   preferencia.remove({"idUser":req.body.elemento.idUser},function (err){});
   preferencia.collection.insert([req.body.elemento],onInsert);
-  res.status(200).jsonp(req.body.elemento);
+  //res.status(200).jsonp(req.body.elemento);
+  res.save(req.body);
 };
 
 function onInsert(err, docs){
