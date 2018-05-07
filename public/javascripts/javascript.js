899d2aea60ae7592d8ee23d5a03d53341e48c0e8
santi <santii.barboza@gmail.com>
2018-05-07 16:14:32 -0300

    css javascript

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,23 +1,21 @@
 
 function cargarArchivoCSS(){
+    document.getElementById('cssArchivo').href = "/stylesheets/Formato2.css";
+}
+function guadarCSSActual(){
     $.get("./api/preferenciasUsuarios?user_id="+dataUser.id, function (data) {
        if(data.existe="true"){
             document.getElementById('cssArchivo').href=data.stylesheets;
        }else{
-            document.getElementById('cssArchivo').href = "/stylesheets/Formato2.css";
+            var archivo=document.getElementById('cssArchivo').href;
+            var elemento_nuevo={idUser:dataUser.id,css:archivo};
+            guardar('./api/preferenciasUsuarios',elemento_nuevo);
         }
     });
-
 }
 
 function cambiarArchivoCss(archivo) {
     document.getElementById('cssArchivo').href = archivo;
-    var elemento_nuevo={idUser:dataUser.id,css:archivo};
-    guardar('./api/preferenciasUsuarios',elemento_nuevo);
-}
-
-function guadarCSSActual(){
-    var archivo=document.getElementById('cssArchivo').href;
     var elemento_nuevo={idUser:dataUser.id,css:archivo};
     guardar('./api/preferenciasUsuarios',elemento_nuevo);
 }
