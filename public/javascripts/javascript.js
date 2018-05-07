d684c1e75507a8002fc83efc3a54dfb80b35ae0b
santi <santii.barboza@gmail.com>
2018-05-07 16:54:05 -0300

    css javascript test

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -5,11 +5,17 @@ function cargarArchivoCSS(){
 function guadarCSSActual(){
     $.get("./api/preferenciasUsuarios?user_id="+dataUser.id, function (data) {
        if(data.status="200"){
+            $("#filtros").append("<h1>Antes de post Status 200"</h1>");
             document.getElementById('cssArchivo').href=data.stylesheet;
+            $("#filtros").append("<h1>Despues de post Status 200"</h1>");
        }else{
+
             var archivo=document.getElementById('cssArchivo').href;
             var elemento_nuevo={idUser:dataUser.id,css:archivo};
+            $("#filtros").append("<h1>Antes de guardar pref"+JSON.stringify(elemento_nuevo)+"</h1>");
             guardar('./api/preferenciasUsuarios',elemento_nuevo);
+            $("#filtros").append("<h1>Despues de guardar pref"+JSON.stringify(elemento_nuevo)+"</h1>");
+
         }
     });
 }
