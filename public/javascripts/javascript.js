3ea2c510433770120c0987eff21326b8840a107c
santi <santii.barboza@gmail.com>
2018-05-07 15:26:02 -0300

    css

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,16 +1,18 @@
 
 function cargarArchivoCSS(){
-    var resultado = localStorage.getItem("css");
-    if (resultado == null) {
-        document.getElementById('cssArchivo').href = "/stylesheets/Formato1.css";
-
-    }else {
-        document.getElementById('cssArchivo').href = resultado;
-        localStorage.setItem('css', resultado);
+    $.get("./api/preferenciasUsuarios", function (data) {
+       if(data.existe="true"){
+            document.getElementById('cssArchivo').href=data.stylesheets;
+       }else{
+            document.getElementById('cssArchivo').href = "/stylesheets/Formato2.css";
+        }
+    });
+    
     }
 }
 
 function cambiarArchivoCss(archivo) {
     document.getElementById('cssArchivo').href = archivo;
-    localStorage.setItem('css', archivo);
+    var elemento_nuevo={idUser:dataUser.id,css:archivo}
+    guardar('./api/css',elemento_nuevo);;
 }
