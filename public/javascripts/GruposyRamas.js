4ef238764b0077ea8942b520402a9a2136806024
rochy22 <rosegovia22@gmail.com>
2018-05-07 13:44:02 -0300

    probando post

--- public/javascripts/GruposyRamas.js
+++ public/javascripts/GruposyRamas.js
@@ -322,7 +322,7 @@ function enviarComentario(){
 
     var Nuevo_comentario =   {id: nick, perteneciente: codigo, texto: comentario, fecha: Fecha, horario: Hora, imagen: Imagen};
 
-    $post('./api/comentarios',Nuevo_comentario);
+    $.post('./api/comentarios',Nuevo_comentario);
   //guardar( Nuevo_comentario);
 
     mostrarComentariosAgregado(nick, comentario, fecha, hora);
