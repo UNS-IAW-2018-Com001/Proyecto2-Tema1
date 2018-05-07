cd73fdacf118e1c8c9d6f27faabc14a7d80a43c3
rochy22 <rosegovia22@gmail.com>
2018-05-07 17:57:49 -0300

    css

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -6,18 +6,19 @@ function guadarCSSActual(){
   $("#filtros").append("<h1>A200</h1>");
 
     $.get("./api/preferenciasUsuario?user_id="+dataUser.id, function (data) {
-
-      $("#filtros").append("<h1>Antes de post Status 200</h1>");
-      document.getElementById('cssArchivo').href=data.stylesheet;
-      $("#filtros").append("<h1>Despues de post Status 200</h1>");
-
-    }).fail(function(){
+      if(data.status==200){
+        $("#filtros").append("<h1>Antes de post Status 200</h1>");
+        document.getElementById('cssArchivo').href=data.stylesheet;
+        $("#filtros").append("<h1>Despues de post Status 200</h1>");
+      }
+    else{
       var archivo=document.getElementById('cssArchivo').href;
       var elemento_nuevo={idUser:dataUser.id,css:archivo};
       $("#filtros").append("<h1>Antes de guardar pref"+JSON.stringify(elemento_nuevo)+"</h1>");
       guardar('./api/preferenciasUsuario',elemento_nuevo);
       $("#filtros").append("<h1>Despues de guardar pref"+JSON.stringify(elemento_nuevo)+"</h1>");
-    });
+    }
+  })
 }
 
 function cambiarArchivoCss(archivo) {
