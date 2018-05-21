7f9bf66f177a3a9bf8c476555d02d9f8150590bb
santi <santii.barboza@gmail.com>
2018-05-21 12:53:48 -0300

    test Filtro

--- app_server/controllers/filtrosController.js
+++ app_server/controllers/filtrosController.js
@@ -49,9 +49,9 @@ const calcularFiltros=function(req,res){
       var queryGrupo=Grupo.find({"_id":filtroRama[rama].GrupoPerteneciente});
       if(filtros["Religion"])
         queryGrupo.where('religion').equals(filtros["Religion"]);
-      queryGrupo.exec((err, grupo) => {
+      queryGrupo.exec((err, grupo,ramita="filtroRama[rama]") => {
         if(grupo){
-        console.log("Rama: "+filtroRama[rama]+", Grupo:"+grupo);
+        console.log("Rama: "+ramita+", Grupo:"+grupo);
         }
       });
     }
