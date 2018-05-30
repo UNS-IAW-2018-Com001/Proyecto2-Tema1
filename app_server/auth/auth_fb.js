ad25d9704529a42202009328ce52de7ec0f4c8e0
rochy22 <rosegovia22@gmail.com>
2018-05-30 14:32:01 -0300

    arreglando dominio

--- app_server/auth/auth_fb.js
+++ app_server/auth/auth_fb.js
@@ -6,14 +6,43 @@ var User = require('../models/user');
 passport.use(new Strategy({
     clientID: '211557066103646',
     clientSecret: '67725dcbcdea15b5ab166a0e8088ae84',
-    callbackURL: 'https://scoutsenargentina.herokuapp.com/auth/facebook/callback'
-  },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate({name: profile.displayName}, {name: profile.displayName,userid: profile.id}, function(err, user) {
-      if (err) { return done(err); }
-      done(null, user);
-    });
-  }
-));
+    callbackURL: "https://scoutsenargentina.herokuapp.com/auth/facebook/callback"
+    profileFields: ['id','displayName','name','photos']
+      },
+      function(accessToken, refreshToken, profile, done) {
+         //check user table for anyone with a facebook ID of profile.id
+        User.findOne({
+            'facebookid': profile.id
+        }, function(err, user) {
+            if (err) {
+                return done(err);
+            }
+            //No user was found... so create a new user with values from Facebook (all the profile. stuff)
+            if (!user) {
+                user = new User({
+                    username: profile.displayName,
+                    facebookid: profile.id,
+                    photo: profile.photos[0].value,
+                    //now in the future searching on User.findOne({'facebook.id': profile.id } will match because of this next line
+                    facebook: profile._json
+                });
+                user.save(function(err) {
+                    if (err) console.log(err);
+                });
+            }
+            //found user. Return
+            return done(null, user);
+
+        });
+      }));
+
+      passport.serializeUser(function(user, cb) {
+        cb(null, user);
+      });
+
+      passport.deserializeUser(function(user, cb) {
+        cb(null, user);
+      });
+
 
 module.exports = passport;
