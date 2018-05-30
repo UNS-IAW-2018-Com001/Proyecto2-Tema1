051bab2abd4a703d7c1269b7c6bc883094d90fce
rochy22 <rosegovia22@gmail.com>
2018-05-30 14:03:53 -0300

    probando login2

--- app_server/auth/auth_fb.js
+++ app_server/auth/auth_fb.js
@@ -6,7 +6,8 @@ var User = require('../models/user');
 passport.use(new Strategy({
     clientID: "211557066103646",
     clientSecret: "67725dcbcdea15b5ab166a0e8088ae84",
-    callbackURL: "https://scoutsenargentina.herokuapp.com"
+    callbackURL: "https: // scoutsenargentina.herokuapp.com / auth / facebook / callback"
+
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOrCreate({name: profile.displayName}, {name: profile.displayName,userid: profile.id}, function(err, user) {
