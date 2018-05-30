265942ca4f976d55bc9419053638fd470ab62980
santi <santii.barboza@gmail.com>
2018-05-30 12:05:55 -0300

    Filtro grupo visual

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -24,36 +24,3 @@ function cambiarArchivoCss(archivo) {
     }
     });
 }
-function filtrar(){
-    var mensaje;
-    var filtrosUsados=new Object();
-    $("#selectsFiltros div button").each(function(){
-        if(!$(this).hasClass("bs-placeholder")){
-            filtrosUsados[this.dataset.id]=this.title;   
-        }
-    });
-    if(filtrosUsados["Edad"]){
-      filtrosUsados["Edad"]=filtrosUsados["Edad"].split(" ")[0];
-    }
-    mensaje=JSON.stringify({filtros: filtrosUsados});
-    getFiltros('./api/filtros/grupos',filtrosUsados);
-    return mensaje;
-}
-
-function getFiltros(ruta,elemento_nuevo) {
-  //const jsonString = JSON.stringify(Array.from(comentario.values()));
-  $.ajax({
-      url: ruta,
-      type: 'POST',
-      data: JSON.stringify({filtros: elemento_nuevo}),
-      contentType: "application/json",
-      dataType: "json",
-      success: function(data){
-        console.log(data);
-      },
-      error:function(data){ }
-  });
-}
-$(document).on("click","#botonFiltro", function(){
-        filtrar();
-});
