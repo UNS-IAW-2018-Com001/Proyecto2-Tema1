2c3cddc9178e0c067a08ad3855a8ba2e33ab55ef
rochy22 <rosegovia22@gmail.com>
2018-06-04 12:45:39 -0300

    login nuevo

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Grupo = mongoose.model('grupos');
 const Filtro = mongoose.model('filtros');
-const User = mongoose.model('user');
+
 
 /* GET home page */
 const index = function(req, res) {
@@ -11,7 +11,7 @@ const index = function(req, res) {
 					title: 'Scouts y Guias en Argentina',
 					filtros: Filtro,
 					grupos: Grupo,
-					user: User
+					user: req.user
 				});
 			})
 		})
