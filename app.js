6f84f7166a779ec2fc77320d7887d32c7bf39cfa
rochy22 <rosegovia22@gmail.com>
2018-06-04 15:32:00 -0300

    solucionando errores

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 const passport = require('passport');
 require('./app_server/models/db')
 
