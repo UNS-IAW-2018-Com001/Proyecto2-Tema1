669660f420168950a013d3e1092b5159c4a1a182
rochy22 <rosegovia22@gmail.com>
2018-06-04 16:23:51 -0300

    login nuevo

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-require('./app_server/models/db')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
+var logger = require('morgan');
+const passport = require('passport');
+
+require('./app_server/models/db');
+
 
 const indexRouter = require('./app_server/routes/index');
 
-const app = express();
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'twig');
-
+app.use(cookieParser());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
+app.use(require('express-session')({
+  secret: 'nodejs-twig-secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/twigjs/twig.min.js',  express.static(__dirname + '/node_modules/twig/twig.min.js'));
+app.use('/shared',  express.static(__dirname + '/app_server/views/shared'));
+
+//routes
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
