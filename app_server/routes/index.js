fa965284a0e9d57a1a870e4ad219feb8ca92c838
rochy22 <rosegovia22@gmail.com>
2018-06-04 16:52:32 -0300

    solucionando errores

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -32,10 +32,10 @@ router.get('/logout/', function(req, res) {
   res.redirect('/');
 });
 
-app.get('/auth/twitter', passport.authenticate('twitter'));
+router.get('/auth/twitter', passport.authenticate('twitter'));
 // Ruta de callback, a la que redirigirá tras autenticarse con Twitter.
 // En caso de fallo redirige a otra vista '/login'
-app.get('/auth/twitter/callback', passport.authenticate('twitter',
+router.get('/auth/twitter/callback', passport.authenticate('twitter',
   { successRedirect: '/', failureRedirect: '/login' }
 ));
 
