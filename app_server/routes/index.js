6aba80bc505bd58ff66f094d6225a15c637dc582
rochy22 <rosegovia22@gmail.com>
2018-06-04 15:25:02 -0300

    login nuevo

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport'); // Passport: Middleware de Node que facilita la autenticación de usuarios
 
 const ctrlMain = require('../controllers/main');
 const ctrlGrupos = require('../controllers/gruposController');
@@ -8,7 +7,7 @@ const ctrlRamas = require('../controllers/ramasController');
 const ctrlComentarios = require('../controllers/comentariosController');
 const ctrlPreferencias = require('../controllers/preferenciasController');
 const ctrlFiltros = require('../controllers/filtrosController');
-
+const passportFacebook = require('../auth/auth_social');
 
 
 /* GET home page. */
@@ -32,17 +31,17 @@ router.get('/logout', function(req, res) {
   res.redirect('/');
 });
 // Ruta para autenticarse con Twitter (enlace de login)
-router.get('/auth/twitter', passport.authenticate('twitter'));
+router.get('/auth/twitter', passportFacebook.authenticate('twitter'));
 // Ruta para autenticarse con Facebook (enlace de login)
-router.get('/auth/facebook', passport.authenticate('facebook'));
+router.get('/auth/facebook', passportFacebook.authenticate('facebook'));
 // Ruta de callback, a la que redirigirá tras autenticarse con Twitter.
 // En caso de fallo redirige a otra vista '/login'
-router.get('/auth/twitter/callback', passport.authenticate('twitter',
+router.get('/auth/twitter/callback', passportFacebook.authenticate('twitter',
   { successRedirect: '/', failureRedirect: '/login' }
 ));
 // Ruta de callback, a la que redirigirá tras autenticarse con Facebook.
 // En caso de fallo redirige a otra vista '/login'
-router.get('/auth/facebook/callback', passport.authenticate('facebook',
+router.get('/auth/facebook/callback', passportFacebook.authenticate('facebook',
   { successRedirect: '/', failureRedirect: '/login' }
 ));
 
