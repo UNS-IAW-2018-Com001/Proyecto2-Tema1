6b3cc47d18255df01d494b23e5ce3859894e7869
rochy22 <rosegovia22@gmail.com>
2018-06-04 16:13:42 -0300

    solucionando errores

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const bodyParser = require('body-parser');
-const passport = require('passport');
 require('./app_server/models/db')
 
 const indexRouter = require('./app_server/routes/index');
@@ -15,26 +13,11 @@ const app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'twig');
 
-
-app.use(cookieParser());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(require('express-session')({
-  secret: 'nodejs-twig-secret',
-  resave: true,
-  saveUninitialized: true
-}));
-
-//passport setup
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-
-
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
