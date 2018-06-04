d0ff663035105d0ce0415e5032227347465aa66c
rochy22 <rosegovia22@gmail.com>
2018-06-04 15:10:58 -0300

    login nuevo

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const passport = require('passport');
-const bodyParser = require('body-parser')
 require('./app_server/models/db')
 
 const indexRouter = require('./app_server/routes/index');
