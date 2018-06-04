ec5340eab206f93d1a0ccaf7138d8f062f9b08d0
rochy22 <rosegovia22@gmail.com>
2018-06-04 12:28:03 -0300

    solucionando errores

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -34,22 +34,22 @@ router.post("/api/filtros/ramas", ctrlFiltros.filtrosRamas);
 
 /* Rutas de Passport */
 // Ruta para desloguearse
-app.get('/logout', function(req, res) {
+router.get('/logout', function(req, res) {
   req.logout();
   res.redirect('/');
 });
 // Ruta para autenticarse con Twitter (enlace de login)
-app.get('/auth/twitter', passport.authenticate('twitter'));
+router.get('/auth/twitter', passport.authenticate('twitter'));
 // Ruta para autenticarse con Facebook (enlace de login)
-app.get('/auth/facebook', passport.authenticate('facebook'));
+router.get('/auth/facebook', passport.authenticate('facebook'));
 // Ruta de callback, a la que redirigirá tras autenticarse con Twitter.
 // En caso de fallo redirige a otra vista '/login'
-app.get('/auth/twitter/callback', passport.authenticate('twitter',
+router.get('/auth/twitter/callback', passport.authenticate('twitter',
   { successRedirect: '/', failureRedirect: '/login' }
 ));
 // Ruta de callback, a la que redirigirá tras autenticarse con Facebook.
 // En caso de fallo redirige a otra vista '/login'
-app.get('/auth/facebook/callback', passport.authenticate('facebook',
+router.get('/auth/facebook/callback', passport.authenticate('facebook',
   { successRedirect: '/', failureRedirect: '/login' }
 ));
 
