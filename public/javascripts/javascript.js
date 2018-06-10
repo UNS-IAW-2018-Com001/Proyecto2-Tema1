8fcc037b7743d5485943ec6b920876aa9ee0d06a
rochy22 <rosegovia22@gmail.com>
2018-06-10 11:33:21 -0300

    eliminando error post

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -8,8 +8,7 @@ function guadarCSSActual(){
     $.get("./api/user?user_id="+dataUser._id, function (data) {
        var archivo=document.getElementById('cssArchivo').href;
       var elemento_nuevo={provider_id: dataUser.provider_id, provider: dataUser.provider, name : dataUser.displayName, photo: dataUser.photo,css:archivo};
-      $("#filtroTag").empty();
-      $("#filtroTag").append (elemento_nuevo);
+
       if(data!=null){
          if(data.css!="sin_estilo"){
            document.getElementById('cssArchivo').href=data.css;
@@ -28,8 +27,6 @@ function cambiarArchivoCss(archivo) {
     document.getElementById('cssArchivo').href = archivo;
     if(dataUser !=null) {
       var elemento_nuevo={provider_id: dataUser.provider_id, provider: dataUser.provider, name : dataUser.displayName, photo: dataUser.photo ,css:archivo};
-      $("#filtroTag").empty();
-      $("#filtroTag").append (elemento_nuevo);
       guardar('./api/user',elemento_nuevo);
     }
 }
